52d3e2aba6b54c71cda66a3fb08dfbd0a45b677a
Prem <premsaini9602@gmail.com>
2026-01-24 14:09:46 +0530

    Cange copyright content

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -34,6 +34,6 @@
     </div>
 
     <div class="text-center text-gray-500 text-sm border-t border-gray-700 py-3">
-        © {{ date('Y') }} News Blog
+        © {{ date('Y') }} Panchayat 365. All rights reserved.
     </div>
 </footer>
