ccc7074cd165961879043bfdde34a8779c4feca3
Prem <premsaini9602@gmail.com>
2026-02-27 12:01:16 +0530

    CSS changes done

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
